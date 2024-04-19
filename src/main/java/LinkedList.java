4ff47e06620fe7fe4948e833a9183a6cf0434da8
sophie <d00259237@student.dkit.ie>
2024-04-19 15:42:19 +0100

    Added appropriate fields and defined the Node class in the LinkedList class. Also added getters and setters.

--- src/main/java/LinkedList.java
+++ src/main/java/LinkedList.java
@@ -1,2 +1,47 @@
 public class LinkedList {
+
+    //Fields
+    private Node head;
+
+    private Node tail;
+
+    private int numberOfAppointments;
+
+
+
+
+
+
+
+    protected static class Node {
+
+        private Appointment data;
+        private Node next;
+
+        public Node(Appointment data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        //Getters and Setters
+
+
+        public Appointment getData() {
+            return data;
+        }
+
+        public void setData(Appointment data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+
 }
