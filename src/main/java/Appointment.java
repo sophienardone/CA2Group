0de7479c5c284f39bca950a0600d095c87249c7f
sophie <d00259237@student.dkit.ie>
2024-04-19 15:03:20 +0100

    Implemented constructor, getters and setters in Appointment class

--- src/main/java/Appointment.java
+++ src/main/java/Appointment.java
@@ -16,4 +16,74 @@ public class Appointment {
     private int triageLevel;
 
     private String docFullName;
+
+
+   //constructor
+    public Appointment(String firstName, String lastName, LocalDate dob, String issue, LocalDate date, int triageLevel, String docFullName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.issue = issue;
+        this.date = date;
+        this.triageLevel = triageLevel;
+        this.docFullName = docFullName;
+    }
+
+    //getters and setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getTriageLevel() {
+        return triageLevel;
+    }
+
+    public void setTriageLevel(int triageLevel) {
+        this.triageLevel = triageLevel;
+    }
+
+    public String getDocFullName() {
+        return docFullName;
+    }
+
+    public void setDocFullName(String docFullName) {
+        this.docFullName = docFullName;
+    }
 }
