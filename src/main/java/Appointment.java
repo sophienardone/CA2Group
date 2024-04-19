422ac2240ac0c58c829cca7567bec2d3f4d5157a
sophie <d00259237@student.dkit.ie>
2024-04-19 12:54:54 +0100

    Added the required fields in Appointment class

--- src/main/java/Appointment.java
+++ src/main/java/Appointment.java
@@ -1,2 +1,19 @@
+import java.time.LocalDate;
+
 public class Appointment {
+
+    //Fields
+    private String firstName;
+
+    private String lastName;
+
+    private LocalDate dob;
+
+    private String issue;
+
+    private LocalDate date;
+
+    private int triageLevel;
+
+    private String docFullName;
 }
