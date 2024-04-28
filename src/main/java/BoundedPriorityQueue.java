99b435d909251982a67a5ff2222754a8c7e3bdb5
sophie <d00259237@student.dkit.ie>
2024-04-28 12:18:02 +0100

    Created a safeAdd method which matches doctor associated with queue before adding appointment

--- src/main/java/BoundedPriorityQueue.java
+++ src/main/java/BoundedPriorityQueue.java
@@ -1,2 +1,28 @@
-public class BoundedPriorityQueue {
+public class BoundedPriorityQueue extends LinkedList {
+
+    private final String doctor;
+
+
+
+    public BoundedPriorityQueue(int maxSize, String doctor) {
+        super(maxSize);
+        this.doctor = doctor;
+
+    }
+
+    public boolean safeAdd(Appointment appointment){
+        if(this.size() < maxSize && appointment.getDocFullName().equalsIgnoreCase(doctor)){
+            add(appointment);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+
+
+
+
 }
