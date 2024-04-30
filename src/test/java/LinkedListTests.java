2948ac901abd93a8b6d56097e1bf072f00d40e1e
sophie <d00259237@student.dkit.ie>
2024-04-30 21:30:00 +0100

    Created size_EmptyList test which checks if the size of the list is 0 when its empty

--- src/test/java/LinkedListTests.java
+++ src/test/java/LinkedListTests.java
@@ -1,2 +1,26 @@
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+
 public class LinkedListTests {
+
+
+    @Test
+    public void testSize_EmptyList() {
+        LinkedList instance = new LinkedList();
+        int expResult = 0;
+        int result = instance.size();
+        assertEquals(expResult, result);
+
+    }
+
+
+
+
+
+
+
+
 }
