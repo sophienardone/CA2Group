d8a1587d310f013a3420016b0ed0058734a483b7
sophie <d00259237@student.dkit.ie>
2024-04-30 22:46:18 +0100

    Added comments and javadocs to BoundedPriorityQueue

--- src/main/java/BoundedPriorityQueue.java
+++ src/main/java/BoundedPriorityQueue.java
@@ -1,13 +1,18 @@
 import java.util.LinkedList;
 public class BoundedPriorityQueue extends LinkedList<Appointment> {
 
+    // Queue to store appointments
     private LinkedList<Appointment> queue;
 
+    //maximum size of queue
     private final int maxSize;
+
+    //doctor associated with the queue
     private final String doctor;
 
 
 
+   //constructor
     public BoundedPriorityQueue(int maxSize, String doctor) {
         this.queue = new LinkedList<>();
         this.maxSize = maxSize;
@@ -16,23 +21,32 @@ public class BoundedPriorityQueue extends LinkedList<Appointment> {
     }
 
 
+
+    // Override size method to return the size of the queue
     @Override
     public int size() {
         return queue.size();
     }
 
+
+    // Override isEmpty method to check if the queue is empty
     @Override
     public boolean isEmpty() {
         return queue.isEmpty();
     }
 
 
-
+    // Method to check if the queue is full
     public boolean isFull() {
         return queue.size() == maxSize;
     }
 
 
+    /**
+     * Adds an appointment to the queue
+     * @param appointment the element to add
+     * @return true if it was added sucessfully, false otherwise
+     */
     public boolean offer(Appointment appointment){
         if(isFull()){
             return false;
@@ -42,8 +56,13 @@ public class BoundedPriorityQueue extends LinkedList<Appointment> {
     }
 
 
+    /**
+     * adds an appointment to the queue in a fail-fast way
+     * @param appointment An appointment object to be added to the queue
+     */
 
     public void failFastAdd(Appointment appointment) {
+
         if (size() >= maxSize ) {
             throw new IllegalStateException("Queue is full");
         }
