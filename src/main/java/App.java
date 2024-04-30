8162a6d980ef003ee27b712a89c24046f74ff9a4
sophie <d00259237@student.dkit.ie>
2024-04-30 18:59:33 +0100

    Created switch case statement and implemented static method menuOptions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,7 +28,52 @@ public class App {
         }
 
 
+        String userChoice = null;
 
+
+        do {
+            menuOptions();
+            System.out.println("Enter your choice");
+            String choice = sc.next();
+            switch (choice) {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+                    System.out.println("Exiting program...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid option. Please choose a valid option.");
+            }
+        } while (true);
+
+
+
+
+
+
+    }
+
+    public static void menuOptions() {
+        System.out.println("Choose an option:");
+        System.out.println("1. Add a new patient");
+        System.out.println("2. Delete a patient");
+        System.out.println("3. Display all patients");
+        System.out.println("4. Create a new appointment for a specific patient");
+        System.out.println("5. Call the next patient in for a specified doctor");
+        System.out.println("6. Exit");
     }
 
 
