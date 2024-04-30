76fb19efc72926c5332152ff27cf84b88f532f7e
sophie <d00259237@student.dkit.ie>
2024-04-30 21:34:10 +0100

    Added populatedList testing and test adding

--- src/test/java/LinkedListTests.java
+++ src/test/java/LinkedListTests.java
@@ -17,6 +17,31 @@ public class LinkedListTests {
     }
 
 
+    @Test
+    public void testSize_PopulatedList() {
+        LinkedList instance = new LinkedList();
+        Appointment a1 = new Appointment("Saoirse", "McGuinness", LocalDate.of(1999, 22,6), "Checkup", LocalDate.now(), 1, "Dr. Curtain");
+        Appointment a2 = new Appointment("Shauna", "Cooney", LocalDate.of(1998, 7, 9), "Bloods", LocalDate.now(), 2, "Dr. McGrath");
+        Appointment a3 = new Appointment("Sophie", "Nardone", LocalDate.of(2000, 28, 2), "Checkup", LocalDate.now(), 3, "Dr. McGrath");
+
+        instance.add(a1);
+        instance.add(a2);
+        instance.add(a3);
+        int expResult = 3;
+        int result = instance.size();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testAdd() {
+        LinkedList instance = new LinkedList();
+        Appointment a1 = new Appointment("John", "Smith", LocalDate.of(2000, 5, 11), "Checkup", LocalDate.now(), 4, "Dr. Reilly");
+        instance.add(a1);
+        assertEquals(1, instance.size());
+    }
+
+
+
 
 
 
